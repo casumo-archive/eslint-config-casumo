2690ec87bca7f13eaf566c32214f7c4329c1c1ae
Luke Camilleri <luke.camilleri@casumo.com>
2016-12-22 03:05:17 +0100

    refactor(rules): Enumerated all eslint rules
    Added all rules according to http://eslint.org/docs/rules/

---
+++ possible-errors.js
@@ -0,0 +1,39 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true
+    },
+    rules: {
+        // NO_RULE 'no-await-in-loop': 2,
+        // 'no-cond-assign': 2,
+        // 'no-console': 2,
+        // 'no-constant-condition': 2,
+        // 'no-control-regex': 2,
+        // 'no-debugger': 2,
+        // 'no-dupe-args': 2,
+        // 'no-dupe-keys': 2,
+        // 'no-duplicate-case': 2,
+        // 'no-empty-character-class': 2,
+        'no-empty': 2,
+        // 'no-ex-assign': 2,
+        // NO_RULE 'no-extra-boolean-cast': 2,
+        // NO_RULE 'no-extra-parens': 2,
+        'no-extra-semi': 2,
+        // 'no-func-assign': 2,
+        // 'no-inner-declarations': 2,
+        // 'no-invalid-regexp': 2,
+        // 'no-irregular-whitespace': 2,
+        // 'no-obj-calls': 2,
+        // NO_RULE 'no-prototype-builtins': 2,
+        // 'no-regex-spaces': 2,
+        // 'no-sparse-arrays': 2,
+        // 'no-template-curly-in-string': 2,
+        // 'no-unexpected-multiline': 2,
+        'no-unreachable': 2
+        // 'no-unsafe-finally': 2,
+        // 'no-unsafe-negation': 2,
+        // 'use-isnan': 2,
+        // 'valid-jsdoc': [ 2, { requireReturn: false } ],
+        // 'valid-typeof': 2
+    }
+};
---
+++ strict-mode.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true
+    },
+    rules: {
+        strict: 0
+    }
+};
