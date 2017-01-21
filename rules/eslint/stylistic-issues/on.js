3ef7dc90dd5eeea299eb938f4b4b60860a6330cd
Csaba Illes <nostrus@gmail.com>
2017-01-21 10:49:30 +0100

    feat: Updated operator-linebreak rule
    Added rule to force usage of operators at the end of lines in case of multi-line operations

--- rules/eslint/stylistic-issues/on.js
+++ rules/eslint/stylistic-issues/on.js
@@ -64,7 +64,7 @@ module.exports = {
         'one-var-declaration-per-line': 0,
         'one-var': [2, { var: 'always', let: 'never', const: 'never' }],
         'operator-assignment': 0,
-        'operator-linebreak': 0,
+        'operator-linebreak': ['error', 'after'],
         'padded-blocks': 0,
         'quote-props': 0,
         quotes: [2, 'single'],
@@ -80,7 +80,6 @@ module.exports = {
         'space-unary-ops': [2, { words: false, nonwords: false }],
         'spaced-comment': 0,
         'unicode-bom': 0,
-        'wrap-regex': 0,
-        'operator-linebreak': ['error', 'after']
+        'wrap-regex': 0
     }
 };
